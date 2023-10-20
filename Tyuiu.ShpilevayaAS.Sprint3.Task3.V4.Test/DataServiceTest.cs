fb4d1e630e6af95da6bd09ef157dd13a57d4f083
Tyuiu.ShpilevayaAS@outlook.com <rina.ress@mail.ru>
2023-10-20 19:32:32 +0500

    Task3

---
+++ Tyuiu.ShpilevayaAS.Sprint3.Task3.V4.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ShpilevayaAS.Sprint3.Task3.V4.Lib;
+
+namespace Tyuiu.ShpilevayaAS.Sprint3.Task3.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidDeleteCharInString()
+        {
+            DataService ds = new DataService();
+
+            string value = "joker has joken";
+            char item = 'j';
+
+            string res = ds.DeleteCharInString(value, item);
+            string wait = "oker has oken";
+
+            Assert.AreEqual(wait, res);
+
+
+        }
+    }
+}
---
+++ Tyuiu.ShpilevayaAS.Sprint3.Task3.V4.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShpilevayaAS.Sprint3.Task3.V4.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShpilevayaAS.Sprint3.Task3.V4.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("98592d75-b71c-4434-a219-8f235dc87d19")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
