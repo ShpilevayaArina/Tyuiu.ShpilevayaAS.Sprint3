7a32d12dc72186a98bb4318aacd67dec3f5127ec
Tyuiu.ShpilevayaAS@outlook.com <rina.ress@mail.ru>
2023-10-23 11:03:34 +0500

    Task5

---
+++ Tyuiu.ShpilevayaAS.Sprint3.Task5.V1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShpilevayaAS.Sprint3.Task5.V1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShpilevayaAS.Sprint3.Task5.V1.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2be8b65e-b220-48fc-9e3b-234f6a0f386d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
